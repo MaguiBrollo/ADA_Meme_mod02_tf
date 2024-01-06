7a95354e16b7221a7d185f9cc7abf411bc259f79
Magui Brollo <maguieb@hotmail.com>
2024-01-05 22:21:28 -0300

    C04 Iconos del navbar.

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ btn_oscuro.addEventListener("click", cambiar_color);
 function cambiar_color(){
     if(btn_oscuro.textContent.trim() === "Modo Claro"){
         document.documentElement.setAttribute("data-theme-color","light");
-        btn_oscuro.innerHTML= `<i class="fa fa-certificate icon-col" aria-hidden="true"> </i> Modo Oscuro`;
+        btn_oscuro.innerHTML= `<i class="fa fa-moon-o" aria-hidden="true"></i> </i> Modo Oscuro`;
     }
     else{
        document.documentElement.setAttribute("data-theme-color","dark"); 
-       btn_oscuro.innerHTML= `<i class="fa fa-certificate icon-col" aria-hidden="true"> </i> Modo Claro`;
+       btn_oscuro.innerHTML= `<i class="fa fa-sun-o"></i> Modo Claro`;
     }
 }
 /* -------------------------------------------------------------------  */
